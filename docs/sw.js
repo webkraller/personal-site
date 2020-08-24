48b4470b3b53f33b12bed67e14aa00899c318a5f
Alex Krall <alex@compound.re>
2020-08-24 15:13:27 -0400

    release

--- docs/sw.js
+++ docs/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5511592dd2a87907f8ca.js"
+    "url": "webpack-runtime-4018ace64a4db0603112.js"
   },
   {
     "url": "framework-a38040c4749cf445737f.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cdcb5d5a56b0c8d4930663c7352c8993"
+    "revision": "e52aa532d874449105bebc9574c497d3"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-5a1ee105f05b3144b807.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "d6ca070bc5b9eb99d19969123f2b9f7a"
+    "revision": "a9999df1992cd0c6e8e9d1681def1d94"
   },
   {
     "url": "polyfill-b4c19884e237349d4e5a.js"
